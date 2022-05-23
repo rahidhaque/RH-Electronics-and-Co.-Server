77bf27f957f5268e15772631ef16f2bfe3910618
rahidhaque <ontirahid@yahoo.com>
2022-05-23 16:09:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello From RH Electronics!!')
+})
+
+app.listen(port, () => {
+    console.log(`RH Electronics app listening on port ${port}`)
+})
