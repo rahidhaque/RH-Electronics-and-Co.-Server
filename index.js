daddfe0d193e3e2803ac838d147a387abec6953e
rahidhaque <ontirahid@yahoo.com>
2022-05-23 17:10:31 +0600

    Mongo DB setup Done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +13,44 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello From RH Electronics!!')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ch01.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run(req, res, next) {
+
+    try {
+        await client.connect();
+        const userCollection = client.db("rh_electronics").collection("users");
+
+        //adding or updating user
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`RH Electronics app listening on port ${port}`)
